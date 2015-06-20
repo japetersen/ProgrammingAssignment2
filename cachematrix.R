fe0d00a67b66133a248980fb6dbe011d6f73f6d1
Julie Petersen <japetersen@ucdavis.edu>
2015-06-19 21:35:22 -0700

    Assignment Upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions together, overall, create a location
+## to store and recall the output of an inverted matrix (or
+## a cached matrix output).
 
-## Write a short comment describing this function
+## The makeCacheMatrix functions creates the location the 
+## cached matrix output gets stored into. It essentially 
+## creates a place holder for the output of the solve
+## function.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m<-NULL
+	set <-function(y){
+		x<<-y
+		m<<-NULL
+	}
+	get<-function() x
+	setinverse <- function(solve) m <<- solve
+	getinverse <- function() m
+	list(set = set, get = get,
+		setinverse = setinverse,
+		getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function returns the inverse of a matrix,
+## but only if there is no output from an earlier call of
+## the function. If there is a result that was cached earlier,
+## it will return that result. Otherwise, cacheSolve will
+## calculate and return the inverse of the matrix entered
+## into the function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	m <- x$getinverse()
+	if(!is.null(m)){
+		message("getting cached data")
+		return(m)
+	}
+	data <- x$get()
+	m <- solve(data)
+	x$setinverse(m)
+	m
 }
